556a18e0960f239ced37628a62ee958e3e427aca
Alson33 <garbuja12alson@gmail.com>
2022-01-05 19:46:40 +0545

    add aos animation for service divs

--- components/home/ServiceSection.tsx
+++ components/home/ServiceSection.tsx
@@ -1,12 +1,14 @@
-
 import styles from '../../styles/home/ServiceSection.module.css'
 
+import 'aos/dist/aos.css'
+import 'aos/dist/aos'
+
 const ServiceSection = (
-    { icon, title }
-    : { icon: string, title: string }
+    { icon, title, duration }
+    : { icon: string, title: string, duration: string}
 ) => {
     return (
-        <div className={styles['service-section']}>
+        <div className={styles['service-section']} data-aos="zoom-in" data-aos-offset="200" data-aos-easing="ease-in-cubic" data-aos-duration={duration}>
             <div className={`${styles['service-icon']} flex`}>
                 {icon}
             </div>
