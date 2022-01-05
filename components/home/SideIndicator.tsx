cd8f772f0063859627a15eb5a1ed25100839a094
Alson33 <garbuja12alson@gmail.com>
2022-01-05 22:11:25 +0545

    add a side indicator for section

---
+++ components/home/SideIndicator.tsx
@@ -0,0 +1,46 @@
+import { useState, useEffect } from 'react'
+import styles from '../../styles/home/SideIndicator.module.css'
+
+const SideIndicator = () => {
+
+    const [currentSection, setCurrentSection] = useState()
+
+    useEffect(() => {
+        window.addEventListener('scroll', () => {
+            console.log(window.pageYOffset);
+            const currentY = window.pageYOffset
+            if(currentY < 500)
+                setCurrentSection(1)
+            else if(currentY < 1250)
+                setCurrentSection(2)
+            else if(currentY < 2100)
+                setCurrentSection(3)
+            else if(currentY < 2800)
+                setCurrentSection(4)
+            else
+                setCurrentSection(5)
+        })
+    }, [])
+
+    return (
+        <div className={styles['side-indicator-holder']}>
+            <div className={`${styles['side-indicators']} ${currentSection===1?styles['active-side-indicator']:''}`}
+                onClick={() => window.scrollTo(0, 0)}
+            ></div>
+            <div className={`${styles['side-indicators']} ${currentSection===2?styles['active-side-indicator']:''}`}
+                onClick={() => window.scrollTo(0, 750)}
+            ></div>
+            <div className={`${styles['side-indicators']} ${currentSection===3?styles['active-side-indicator']:''}`}
+                onClick={() => window.scrollTo(0, 1500)}
+            ></div>
+            <div className={`${styles['side-indicators']} ${currentSection===4?styles['active-side-indicator']:''}`}
+                onClick={() => window.scrollTo(0, 2240)}
+            ></div>
+            <div className={`${styles['side-indicators']} ${currentSection===5?styles['active-side-indicator']:''}`}
+                onClick={() => window.scrollTo(0, 3000)}
+            ></div>
+        </div>
+    )
+}
+
+export default SideIndicator
--- components/home/index.js
+++ components/home/index.js
@@ -1,2 +1,3 @@
 export { default as ServiceSection } from './ServiceSection.tsx'
 export { default as TestomonialSection } from './TestomonialSection.tsx'
+export { default as SideIndicator } from './SideIndicator.tsx'
